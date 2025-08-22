27dbafe5687b12cab5da5c74772a0845b11d29d8
User <you@example.com>
2025-08-22 21:58:19 +0630

    d-board

--- src/pages/DashboardPage.jsx
+++ src/pages/DashboardPage.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import { useTasks } from "../context/TaskContext";
 import BoardPage from "./BoardPage";
+import "../styles/dashBoard.css";
 
 const DashboardPage = () => {
   const { tasks } = useTasks();
@@ -15,16 +16,16 @@ const DashboardPage = () => {
     <div className="container mt-4">
       <h2 className="text-center">Dashboard</h2>
 
-      <div className="row text-center mb-4">
-        <div className="col">
+      <div className="dashboard-summary-row">
+        <div className="dashboard-summary-col">
           <h5>To Do</h5>
           <p>{summary.todo}</p>
         </div>
-        <div className="col">
+        <div className="dashboard-summary-col">
           <h5>In Progress</h5>
           <p>{summary.inprogress}</p>
         </div>
-        <div className="col">
+        <div className="dashboard-summary-col">
           <h5>Done</h5>
           <p>{summary.done}</p>
         </div>
