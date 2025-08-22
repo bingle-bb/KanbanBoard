8237f5c58ca3a4f2f00457b0e3a49fc1e1d7994f
revy <revy200311@gmail.com>
2025-08-23 00:00:36 +0630

    style fix2

--- src/components/Layout.jsx
+++ src/components/Layout.jsx
@@ -2,12 +2,13 @@ import React from "react";
 import { Outlet } from "react-router-dom";
 import Sidebar from "./Sidebar";
 import "../styles/Layout.css";
+import DashboardPage from "../pages/DashboardPage";
 
 const Layout = () => (
   <div style={{ display: "flex" }}>
     <Sidebar />
     <main className="main-content">
-      <Outlet />
+      <DashboardPage />
     </main>
   </div>
 );
